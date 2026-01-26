43ea22f3c04f00536720d5271a9d77f787ccec8c
Killer-oooo <braned1988@gmail.com>
2026-01-26 06:42:59 +0100

    Add autofarm resume tracking to UserData

--- TBot/Services/UserData.cs
+++ TBot/Services/UserData.cs
@@ -9,31 +9,36 @@ using TBot.Ogame.Infrastructure.Models;
 
 namespace Tbot.Services {
 
-	// Data required by TBotMain instances
-	public class UserData {
-		public Server serverInfo = new();
-		public ServerData serverData;
-		public UserInfo userInfo;
-		public AllianceClass allianceClass;
-		public List<Celestial> celestials;
-		public List<Fleet> fleets;
-		public List<AttackerFleet> attacks;
-		public Slots slots;
-		public Researches researches;
+    // Data required by TBotMain instances
+    public class UserData {
+        public Server serverInfo = new();
+        public ServerData serverData;
+        public UserInfo userInfo;
+        public AllianceClass allianceClass;
+        public List<Celestial> celestials;
+        public List<Fleet> fleets;
+        public List<AttackerFleet> attacks;
+        public Slots slots;
+        public Researches researches;
 
-		public List<FleetSchedule> scheduledFleets;
-		public List<FarmTarget> farmTargets;
-		public Dictionary<Coordinate, DateTime> discoveryBlackList;
-		public float lastDOIR;
-		public float nextDOIR;
-		public Staff staff;
-		public bool isSleeping = false;
-	}
+        public List<FleetSchedule> scheduledFleets;
+        public List<FarmTarget> farmTargets;
+        public Dictionary<Coordinate, DateTime> discoveryBlackList;
+        public float lastDOIR;
+        public float nextDOIR;
+        public Staff staff;
+        public bool isSleeping = false;
 
-	// Data used by TelegramMessenger binded to TBotMain
-	public class TelegramUserData {
-		public Celestial CurrentCelestial;			// Willingly left to null
-		public Celestial CurrentCelestialToSave;	// Willingly left to null
-		public Missions Mission = Missions.None;
-	}
+        // AutoFarm resume position - continues scanning from where it left off
+        public int autoFarmLastGalaxy = 0;
+        public int autoFarmLastSystem = 0;
+		public int autoFarmLastRangeIndex = 0;
+    }
+
+    // Data used by TelegramMessenger binded to TBotMain
+    public class TelegramUserData {
+        public Celestial CurrentCelestial;            // Willingly left to null
+        public Celestial CurrentCelestialToSave;    // Willingly left to null
+        public Missions Mission = Missions.None;
+    }
 }
